ce92ded9fc0cacea5b226b99e8f21d15e25ec3e8
Justine <saytin009@gmail.com>
2018-10-12 20:30:33 +0800

    First Commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+          <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="sr-only"></span>
+            </button>
+      <a class="navbar-brand" href="/">{{config('app.name', 'Blog')}}</a>
+
+    </div>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar-nav">
+          <li class=" active">
+            <a href="/">Home
+            </a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+<!-- layouts.app - layouts because it is in the layouts folder and the name of the file is app -->
+@extends('layouts.app')
+@section('content')
+<h1>{{$title}}</h1>   
+    <p>About File</p>
+@endsection
